b82c375e5e0d63eed261d38709cee44c04965260
DeathQuake <mnn.thomas@gmail.com>
2020-10-20 19:46:23 +0530

    Initial commit

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotate : MonoBehaviour
+{
+    public float rotatespeed = 10f;
+    private float mStartingPosition;
+
+    void Update()
+    {
+#if UNITY_EDITOR
+        MouseRotation();
+#else
+        TouchRotation();
+#endif  
+    }
+
+
+    private void MouseRotation()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            mStartingPosition = Input.mousePosition.x;
+        }
+        else if(Input.GetMouseButton(0))
+        {
+            if (mStartingPosition > Input.mousePosition.x)
+            {
+                transform.Rotate(Vector3.up, -rotatespeed * Time.deltaTime);
+            }
+            else if (mStartingPosition < Input.mousePosition.x)
+            {
+                transform.Rotate(Vector3.up, rotatespeed * Time.deltaTime);
+            }
+        }
+    }
+    private void TouchRotation()
+    {
+        if (Input.touchCount > 0)
+        {
+            Touch touch = Input.GetTouch(0);
+            switch (touch.phase)
+            {
+                case TouchPhase.Began:
+                    mStartingPosition = touch.position.x;
+                    break;
+                case TouchPhase.Moved:
+                    if (mStartingPosition > touch.position.x)
+                    {
+                        transform.Rotate(Vector3.up, -rotatespeed * Time.deltaTime);
+                    }
+                    else if (mStartingPosition < touch.position.x)
+                    {
+                        transform.Rotate(Vector3.up, rotatespeed * Time.deltaTime);
+                    }
+                    break;
+                case TouchPhase.Ended:
+                    Debug.Log("Touch Phase Ended.");
+                    break;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/ObRotate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObRotate : MonoBehaviour
+{
+    [SerializeField] private Vector3 m_RotationVector;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(m_RotationVector.x * Time.deltaTime, m_RotationVector.y * Time.deltaTime, m_RotationVector.z * Time.deltaTime));
+    }
+}
